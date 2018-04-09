c5c97504dd92c79955e5552133edcdf024d88407
Jeroen Bransen <jeroen@chordify.net>
2018-04-09 16:10:03 +0200

    Alter migration script to our needs

--- app/migrations/Version20180409081450.php
+++ app/migrations/Version20180409081450.php
@@ -5,9 +5,6 @@ namespace Mautic\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20180409081450 extends AbstractMigration
 {
     /**
@@ -15,10 +12,8 @@ class Version20180409081450 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE form_results_1_test_notif');
         $this->addSql('ALTER TABLE push_ids ADD type INT NOT NULL');
     }
 
@@ -27,10 +22,8 @@ class Version20180409081450 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE form_results_1_test_notif (submission_id INT NOT NULL, form_id INT NOT NULL, email LONGTEXT DEFAULT NULL COLLATE utf8_unicode_ci, UNIQUE INDEX UNIQ_1F6C1CD2E1FD49335FF69B7D (submission_id, form_id), PRIMARY KEY(submission_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE push_ids DROP type');
     }
 }
