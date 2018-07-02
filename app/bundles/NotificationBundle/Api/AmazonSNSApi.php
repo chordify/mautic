86663b3f79a424fd6e59966b04087487ab89b002
Jeroen Bransen <jeroen@chordify.net>
2018-07-02 09:50:52 +0200

    Properly parse 'Enabled' field for Amazon's getEndpointAttributes (fixes #11)

--- app/bundles/NotificationBundle/Api/AmazonSNSApi.php
+++ app/bundles/NotificationBundle/Api/AmazonSNSApi.php
@@ -11,17 +11,16 @@
 
 namespace Mautic\NotificationBundle\Api;
 
-use Aws\Sns\SnsClient;
-use Aws\Sns\Exception\NotFoundException;
 use Aws\Common\Credentials\Credentials;
+use Aws\Sns\Exception\NotFoundException;
+use Aws\Sns\SnsClient;
 use Mautic\NotificationBundle\Entity\Notification;
 use Mautic\NotificationBundle\Entity\PushID;
 
 class AmazonSNSApi extends AbstractNotificationApi
 {
-
     /**
-     * @param array        $playerIds     Array of pushID's
+     * @param array        $playerIds    Array of pushID's
      * @param Notification $notification
      *
      * @return bool
@@ -38,45 +37,47 @@ class AmazonSNSApi extends AbstractNotificationApi
         $keySecret       = $apiKeys['key_secret'];
         $region          = $apiKeys['region'];
 
-        $message = [ 'default' => $notification->getMessage() ];
+        $message = ['default' => $notification->getMessage()];
         if ($notification->isMobile()) {
             $this->addMobileData($message, $notification);
         }
-        $messageData = [ 'MessageStructure' => 'json',
-                         'Subject' => $notification->getHeading(),
-                         'Message' => json_encode($message) ];
+        $messageData = ['MessageStructure' => 'json',
+                         'Subject'         => $notification->getHeading(),
+                         'Message'         => json_encode($message), ];
 
         $credentials = new Credentials($keyID, $keySecret);
-        $client = SnsClient::factory(['credentials' => $credentials,
-                                      'region' => $region,
-                                      'version' => 'latest']);
+        $client      = SnsClient::factory(['credentials' => $credentials,
+                                      'region'           => $region,
+                                      'version'          => 'latest', ]);
 
         $sent = false;
-        foreach($playerIds as $playerId) {
-            if(!$this->ensureArn($featureSettings, $client, $playerId)) {
+        foreach ($playerIds as $playerId) {
+            if (!$this->ensureArn($featureSettings, $client, $playerId)) {
                 $model = $this->factory->getModel('lead.lead');
                 $model->removePushIDFromLead($playerId->getLead(), $playerId);
                 continue;
             }
 
             $messageData['TargetArn'] = $playerId->getAmazonArn();
-            $published = $client->publish($messageData);
+            $published                = $client->publish($messageData);
             if (MAUTIC_ENV === 'dev') {
                 print_r($messageData);
             }
             $sent = true;
         }
+
         return $sent;
     }
 
-    private function ensureArn($featureSettings, SnsClient $client, PushID $pushId ) {
+    private function ensureArn($featureSettings, SnsClient $client, PushID $pushId)
+    {
         $em         = $this->factory->get('doctrine.orm.entity_manager');
         $pushIDRepo = $em->getRepository(PushID::class);
 
-        if( $pushId->getAmazonArn() == null ){
+        if ($pushId->getAmazonArn() == null) {
             // Get the App Arn
             $appArn = null;
-            switch($pushId->getType()) {
+            switch ($pushId->getType()) {
             case PushID::TYPE_APPLE_DEV:
                 $appArn = $featureSettings['apple_dev_arn'];
                 break;
@@ -92,12 +93,12 @@ class AmazonSNSApi extends AbstractNotificationApi
             $endpoint = $client->createPlatformEndpoint(
                 [
                     'PlatformApplicationArn' => $appArn,
-                    'Token' => $pushId->getPushID(),
+                    'Token'                  => $pushId->getPushID(),
                 ]);
 
             // Save it
             $pushId->setAmazonArn($endpoint['EndpointArn']);
-            if($pushId->getId() != null) {
+            if ($pushId->getId() != null) {
                 $pushIDRepo->saveEntity($pushId);
             }
         }
@@ -105,33 +106,35 @@ class AmazonSNSApi extends AbstractNotificationApi
         // Check the attributes
         try {
             $attributes = $client->getEndpointAttributes(['EndpointArn' => $pushId->getAmazonArn()]);
-            return $attributes['Attributes']['Enabled'];
+
+            return json_decode($attributes['Attributes']['Enabled']);
         } catch (NotFoundException $e) {
             // Arn not valid anymore
             return false;
         }
     }
 
-    private function addMobileData(array &$message, Notification $notification) {
+    private function addMobileData(array &$message, Notification $notification)
+    {
         $mobileConfig = $notification->getMobileSettings();
 
         // iOS fields
         $apsFields = [
             'alert' => [
                 'title' => $notification->getHeading(),
-                'body' => $notification->getMessage(),
+                'body'  => $notification->getMessage(),
             ],
         ];
         $apsFields['sound'] = empty($mobileConfig['ios_sound']) ? 'default' : $mobileConfig['ios_sound'];
-        if( isset($mobileConfig['ios_badgeCount']) ) {
+        if (isset($mobileConfig['ios_badgeCount'])) {
             $apsFields['badge'] = (int) $mobileConfig['ios_badgeCount'];
         }
         $iosFields = [
-            'aps' => $apsFields,
+            'aps'             => $apsFields,
             'notification_id' => $notification->getId(),
         ];
         $message['APNS_SANDBOX'] = json_encode($iosFields);
-        $message['APNS'] = json_encode($iosFields);
+        $message['APNS']         = json_encode($iosFields);
 
         // Android fields
         $androidFields = [
